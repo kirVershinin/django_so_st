32e71a9512b2621dfafcc89d4dc891e2c8548517
elc <kirillver359@gmail.com>
2018-07-25 04:57:40 +0300

    upd

--- local_init.sh
+++
@@ -1,5 +0,0 @@
-sudo ln -sf /home/elc/Documents/PyPrac/CoursePrac/Stepik_Web/web_app/etc/nginx.conf  /etc/nginx/
-sudo /usr/bin/nginx
-#sudo ln -sf $(pwd)/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn restart
-gunicorn hello:app -b localhost:8080 &
