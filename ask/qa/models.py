7d642c60498029234df1139b890dd8396598e36d
elc <kirillver359@gmail.com>
2018-07-27 19:45:42 +0300

    models updt

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class QuestionManager(models.Manager):
+	def new():
+		return self.order_by('-added_at')
+	def popular():
+		return self.order_by('-rating')
+
+
 class Question(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
@@ -8,13 +15,6 @@ class Question(models.Model):
 	rating = models.IntegerField(default = 0)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	likes = models.ManyToManyField(User, related_name='likes_set')
-
-	class QuestionManager(models.Manager):
-		def new():
-			return self.order_by('-added_at')
-		def popular():
-			return self.order_by('-rating')
-
 	objects = QuestionManager()		
 	
 	def __str__ (self):
