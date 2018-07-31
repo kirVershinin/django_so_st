577173302cef0bee2f0858d2d7920512b43ca4e8
elc <kirillver359@gmail.com>
2018-07-31 03:01:29 +0300

    views updt

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('', views.latest, name='latest'),
+	path('popular/', views.popular, name='popular'),
+	path('question/<question_id>/', views.question, name='question'),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from .models import Question, Answer
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+def latest(request):
+	qu_list = Question.objects.all().order_by('new')
+	paginator = Paginator(qu_list, 10)
+	page = request.GET.get('page')
+	question = paginator.get_page(page)	
+	return render(request, 'latest.html', {
+		'question': question})
+							
+def popular(request):
+	pop_list = Question.objects.all().order_by('popular')
+	paginator = Paginator(pop_list, 10)
+	page = request.GET.get('page')
+	popular = paginator.get_page(page)
+	return render(request, 'popular.html', {
+		'popular':popular})
 
+def question(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	answer = Answer.objects.all()
+	return render(request, 'qa/question.html', {
+		'question':question,
+		'answer': answer,
+	})
