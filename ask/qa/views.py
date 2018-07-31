265617115c914ca1610dfc9e2131bbf1053cac0e
elc <kirillver359@gmail.com>
2018-07-31 19:36:01 +0300

    views updt

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,25 +4,23 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 def latest(request):
-	qu_list = Question.objects.all().order_by('new')
+	qu_list = Question.objects.all().order_by('-added_at')
 	paginator = Paginator(qu_list, 10)
 	page = request.GET.get('page')
 	question = paginator.get_page(page)	
 	return render(request, 'latest.html', {
-		'question': question})
+		'question':question})
 							
 def popular(request):
-	pop_list = Question.objects.all().order_by('popular')
+	pop_list = Question.objects.all().order_by('-rating')
 	paginator = Paginator(pop_list, 10)
 	page = request.GET.get('page')
-	popular = paginator.get_page(page)
+	question = paginator.get_page(page)
 	return render(request, 'popular.html', {
-		'popular':popular})
+		'question':question})
 
 def question(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	answer = Answer.objects.all()
-	return render(request, 'qa/question.html', {
-		'question':question,
-		'answer': answer,
-	})
+	return render(request, 'question.html', {
+		'question':question})
